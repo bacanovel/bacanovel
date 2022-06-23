b07fc56d5e206281a15b4b4d1393a0ce8737ce00
satyaamanda <satyaaamanda@gmail.com>
2022-06-23 13:45:19 +0700

    migration

---
+++ migrations/20220623064057-create-subscribe.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Subscribes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      NovelId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Novels",
+          key: "id"
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE"
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Subscribes');
+  }
+};
