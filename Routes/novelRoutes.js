6257c4cd9da230454c61961b6886530d474cee6b
alirezaG0104 <alireza0104@gmail.com>
2022-06-23 11:13:50 +0700

    adding data novel, Users, and useridentity

---
+++ Routes/novelRoutes.js
@@ -0,0 +1,6 @@
+// const express = require('express')
+// const router = express.Router()
+// const novelController = require('../Controllers/novelController')
+
+// router.get('/', novelController.novelList)
+// module.exports = router
